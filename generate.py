ee0bf0ac809def783cb4fef90fb00592b211662c
zerokanjana <33819062+zerokanjana@users.noreply.github.com>
2019-04-21 10:17:29 +0700

    Update generate.py

--- generate.py
+++ generate.py
@@ -2,37 +2,12 @@
 
 import random
 
-_subject    = ["ฉัน","คุณ","เขา","มัน","พวกเรา","พวกเขา","คน","ชาวบ้าน"]
-ext_sub     = [None,"ที่สูงๆ","ที่กวาดถนนอยู่","ตรงนั้น","ในห้องนั้น"]
+_subject    = ["ฉัน","คุณ","เขา","มัน","พวกเรา","พวกเขา","คน","ชาวบ้าน","you","she","they","he"]
+ext_sub     = [None,"ที่สูงๆ","ที่กวาดถนนอยู่","ตรงนั้น","ในห้องนั้น","fat",]
 _verb       = ["กำลังวิ่ง","เดิน","คุยกัน","นอนอยู่"]
-ext_verb    = [None,"อย่างรวดเร็ว","ช้าๆ","เสียงดังมาก"]
+ext_verb    = [None,"อย่างรวดเร็ว","ช้าๆ","เสียงดังมาก"]ั
 _object     = [None,"ในลู่วิ่ง","เรื่อง Python ","บนบาทวิถี","ใต้ต้นไม้"]
 ext_obj     = [None,"ที่สนามกีฬา","ที่มีไม้ปกคลุม","ยักษ์ใหญ่"]
-
-file_words = str(open(r"words\subject.txt","r"))
-new_list = file_words.split("\n")
-for word in new_list:
-    _subject.append(word)
-file_words = str(open(r"words\verb.txt","r"))
-new_list = file_words.split("\n")
-for word in new_list:
-    _verb.append(word)
-file_words = str(open(r"words\object.txt","r"))
-new_list = file_words.split("\n")
-for word in new_list:
-    _object.append(word)
-file_words = str(open(r"words\extend subject.txt","r"))
-new_list = file_words.split("\n")
-for word in new_list:
-    _subject.append(word)
-file_words = str(open(r"words\extend verb.txt","r"))
-new_list = file_words.split("\n")
-for word in new_list:
-    _verb.append(word)
-file_words = str(open(r"words\extend object.txt","r"))
-new_list = file_words.split("\n")
-for word in new_list:
-    _object.append(word)
 
 def make(_part):
 
